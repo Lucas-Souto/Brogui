8728d8e4f961a49da735af68d40800e5c82d3b92
Lucas Souto <contato.lucassouto@outlook.com>
2022-07-12 08:07:32 -0300

    Correção no list do comments.js.

--- db/comments.js
+++ db/comments.js
@@ -10,7 +10,11 @@ exports.insert = (author, content, postId, mainId = null, callback = defaultCall
 
 exports.list = (postId, mainId = null, minDate = 0, limit = 16, callback = defaultCallback) =>
 {
-    db.run(`SELECT * FROM comments WHERE date > ? AND postId = ? AND (mainId = ? OR mainId IS NULL) ORDER BY date LIMIT ?`, [minDate, postId, mainId, limit], callback);
+    db.run(`SELECT Comments.*,
+        Users.photo as authorPhoto, Users.name as authorName FROM comments 
+        INNER JOIN users ON Comments.author = Users.username
+        WHERE date > ? AND postId = ? AND ${ mainId == null ? 'mainId IS NULL' : 'mainId = ?' } ORDER BY date LIMIT ?`, 
+    mainId == null ? [minDate, postId, limit] : [minDate, postId, mainId, limit], callback);
 }
 
 exports.delete = (id, callback = defaultCallback) =>
